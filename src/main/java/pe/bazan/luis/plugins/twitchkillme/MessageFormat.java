5ddb2c6b66c57a9cff3821c26c400dda3067e0eb
Luis Bazán <luis@bazan.pe>
2023-01-29 21:40:35 -0500

    Fix a buf on MessageFormat

--- src/main/java/pe/bazan/luis/plugins/twitchkillme/MessageFormat.java
+++ src/main/java/pe/bazan/luis/plugins/twitchkillme/MessageFormat.java
@@ -5,9 +5,11 @@ import org.bukkit.ChatColor;
 
 public class MessageFormat {
   public static Component formatMC(String text) {
+    if(text == null) return Component.text("");
     return Component.text(ChatColor.translateAlternateColorCodes('&', text));
   }
   public static String formatMCTxt(String text) {
+    if(text == null) return "";
     return ChatColor.translateAlternateColorCodes('&', text);
   }
 }
